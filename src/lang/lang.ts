1ffeb5a336ce1f0e8a812b4c4daa5415909751a1
Dibeo <115413042+Dibeo@users.noreply.github.com>
2025-01-20 17:34:52 +0100

    separate lang

---
+++ src/lang/lang.ts
@@ -0,0 +1,6 @@
+const languages = [
+    { code: 'fr', label: 'Français', flag: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg' },
+    { code: 'en', label: 'English', flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg' },
+  ];
+
+export default languages;
