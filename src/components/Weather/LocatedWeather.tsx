9a752d3aa8344abb06e237692bf11774165f3dc3
Dibeo <115413042+Dibeo@users.noreply.github.com>
2025-01-24 17:40:39 +0100

    now can choose a city

---
+++ src/components/Weather/LocatedWeather.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import {
+  CircularProgress,
+  IconButton,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import {
+  CardStyled,
+  CardBodyStyled,
+} from "../../styles/Components/Weather/WeatherCard";
+import SearchIcon from "@mui/icons-material/Search";
+import { useTranslation } from "react-i18next";
+
+interface WeatherData {
+  name: string;
+  main: { temp: number };
+  weather: { description: string; icon: string }[];
+  sys: { country: string; sunrise: number; sunset: number };
+}
+
+interface WeatherProps {
+  latitude: number;
+  longitude: number;
+  apiKey: string;
+  onSearch: (searchTerm: string) => void; // Fonction pour envoyer au parent
+}
+
+const LocatedWeather: React.FC<WeatherProps> = ({
+  latitude,
+  longitude,
+  apiKey,
+  onSearch, // fonction passée par le parent
+}) => {
+  const { t, i18n } = useTranslation();
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [showCard, setShowCard] = useState(false);
+  const [searchTerm, setSearchTerm] = useState<string>(""); // État local pour l'input
+
+  useEffect(() => {
+    const currentLang = i18n.language;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=${currentLang}`;
+
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Failed to fetch weather data");
+        }
+        const data: WeatherData = await response.json();
+        setWeather(data);
+        setShowCard(true);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, [latitude, longitude, apiKey, i18n.language]);
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  if (loading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px", color: "white" }}>
+        <CircularProgress />
+        <Typography variant="h6" gutterBottom>
+          {t("loading_current")}...
+        </Typography>
+      </div>
+    );
+  }
+
+  if (!weather) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px", color: "white" }}>
+        <Typography variant="h6" gutterBottom>
+          {t("error_loading_weather")}
+        </Typography>
+      </div>
+    );
+  }
+
+  return (
+    <CardStyled show={showCard}>
+      <CardBodyStyled>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder={t("search")}
+          value={searchTerm}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+          style={{ color: "white" }}
+        />
+        <IconButton
+          style={{ color: "white" }}
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="search"
+          onClick={handleSearch}
+        >
+          <SearchIcon />
+        </IconButton>
+      </CardBodyStyled>
+    </CardStyled>
+  );
+};
+
+export default LocatedWeather;
--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -4,12 +4,78 @@ import ForecastWeather from "../components/Weather/ForecastWeather";
 import Swal from "sweetalert2";
 import { useTranslation } from "react-i18next";
 import "../styles/pages/Weather.css";
+import LocatedWeather from "../components/Weather/LocatedWeather";
 
 const Weather: React.FC = () => {
   const { t } = useTranslation();
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
   const [apiKey] = useState<string>(import.meta.env.VITE_WEATHER_API_KEY);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  // Recherche de la ville en fonction du nom
+  const searchCity = async (city: string) => {
+    try {
+      const geocodingUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+      const response = await fetch(geocodingUrl);
+      const data = await response.json();
+
+      if (data.cod === 200) {
+        setLatitude(data.coord.lat);
+        setLongitude(data.coord.lon);
+      } else {
+        Swal.fire({
+          title: t("error"),
+          text: t("city_not_found"),
+          imageUrl: "/no-location.png",
+          imageWidth: 300,
+          imageHeight: 300,
+          imageAlt: "City not found",
+          customClass: {
+            popup: "glassmorphic-popup",
+            title: "white-text",
+            htmlContainer: "white-text",
+          },
+          background: "rgba(255, 255, 255, 0.4)",
+          willOpen: () => {
+            const popup = Swal.getPopup();
+            popup!.style.background = "rgba(255, 255, 255, 0.4)";
+            popup!.style.backdropFilter = "blur(10px)";
+            popup!.style.borderRadius = "20px";
+            popup!.style.border = "none";
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching city data:", error);
+      Swal.fire({
+        title: t("error"),
+        text: t("network_error"),
+        imageUrl: "/no-location.png",
+        imageWidth: 300,
+        imageHeight: 300,
+        imageAlt: "Error",
+        customClass: {
+          popup: "glassmorphic-popup",
+          title: "white-text",
+          htmlContainer: "white-text",
+        },
+        background: "rgba(255, 255, 255, 0.4)",
+        willOpen: () => {
+          const popup = Swal.getPopup();
+          popup!.style.background = "rgba(255, 255, 255, 0.4)";
+          popup!.style.backdropFilter = "blur(10px)";
+          popup!.style.borderRadius = "20px";
+          popup!.style.border = "none";
+        },
+      });
+    }
+  };
+
+  const handleSearch = (search: string) => {
+    setSearchTerm(search);
+    searchCity(search);
+  };
 
   const getLocation = () => {
     navigator.geolocation.getCurrentPosition(
@@ -55,18 +121,17 @@ const Weather: React.FC = () => {
       </div>
     );
   }
+
   return (
     <>
-      <CurrentWeather
-        latitude={latitude}
-        longitude={longitude}
-        apiKey={apiKey}
-      />
-      <ForecastWeather
-        latitude={latitude}
-        longitude={longitude}
-        apiKey={apiKey}
-      />
+      {latitude && longitude && (
+        <>
+          <CurrentWeather latitude={latitude} longitude={longitude} apiKey={apiKey} />
+          <ForecastWeather latitude={latitude} longitude={longitude} apiKey={apiKey} />
+        </>
+      )}
+
+      <LocatedWeather latitude={latitude} longitude={longitude} apiKey={apiKey} onSearch={handleSearch} />
     </>
   );
 };
