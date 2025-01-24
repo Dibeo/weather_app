8d09d8bb0e5217e51f4f1ef488e0e5b03bfcb1fa
Dibeo <115413042+Dibeo@users.noreply.github.com>
2025-01-24 19:41:25 +0100

    try to fix vexel #2

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
